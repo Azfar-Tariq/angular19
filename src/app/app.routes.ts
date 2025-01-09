c9816d06ad5188d170e427ea1d1a0c053bf4b641
Azfar Tariq <azfaro2031@gmail.com>
2025-01-09 15:35:44 +0500

    task-commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,42 +1,7 @@
 import { Routes } from '@angular/router';
-import { AdminComponent } from './components/admin/admin.component';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
-import { NgClassComponent } from './components/ng-class/ng-class.component';
-import { NgStyleComponent } from './components/ng-style/ng-style.component';
-import { ControlStatementComponent } from './components/control-statement/control-statement.component';
-import { SignalComponent } from './components/signal/signal.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'data-binding',
-    pathMatch: 'full',
-  },
-  {
-    path: 'admin',
-    component: AdminComponent,
-  },
-  {
-    path: 'data-binding',
-    component: DataBindingComponent,
-  },
-  {
-    path: 'ng-class',
-    component: NgClassComponent,
-  },
-  {
-    path: 'ng-style',
-    component: NgStyleComponent,
-  },
-  {
-    path: 'control-statement',
-    component: ControlStatementComponent,
-  },
-  {
-    path: 'signal',
-    component: SignalComponent,
-  },
   {
     path: 'task-list',
     component: TaskListComponent,
